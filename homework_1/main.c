137e6059168eba89366aafd35c591bf66a4f0537
Furkan URAL <uralfrkn@gmail.com>
2016-10-19 20:42:19 +0300

    Initial Commit

---
+++ homework_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  homework_1
+//
+//  Created by Furkan Ural on 19/10/2016.
+//  Copyright © 2016 Furkan Ural. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
